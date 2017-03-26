87c3a613af3d1164e1d965256b63c5e09f77a79b
Karthik Balakrishnan <karthik.b@gatech.edu>
2017-03-26 13:59:15 -0400

    Created website splitter shell script

---
+++ split_list.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage is bash split.sh filename num_of_split";
+fi
+
+file=$1
+max_split=$2
+
+i=0
+while read -r line; do
+    ((i++))
+    echo $line >> 'website_list_'$i
+    # echo $line' '$i
+    if [[ $i -eq $max_split ]]; then 
+	i=0
+    fi
+done < $file
