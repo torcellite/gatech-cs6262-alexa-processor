35455447ce583a4f42404224d766dd43d6c15a4d
Karthik Balakrishnan <karthik.b@gatech.edu>
2017-03-27 14:11:40 -0400

    Edited code to use 8 docker instances

--- collect_data.sh
+++ collect_data.sh
@@ -4,7 +4,7 @@ DATE=`date +"%m-%d-%y"`
 
 mkdir -p crawled_websites/$DATE
 
-for i in {1..6}; do
+for i in {1..8}; do
     docker cp crawler_container_$i:/crawler/crawled_websites crawled_websites/$DATE/crawled_websites
     mv crawled_websites/$DATE/crawled_websites/* crawled_websites/$DATE
     rm -r crawled_websites/$DATE/crawled_websites
@@ -12,3 +12,6 @@ for i in {1..6}; do
     docker rm crawler_container_$i
     docker run -d -t --name crawler_container_$i crawler_image
 done;
+
+# Ensure everyone has access
+chown kbalakrishnan8:gtperson -R crawled_websites
