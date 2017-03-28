06991bc08c67bf736eb6a5d1bb840ae1e4f10934
Karthik Balakrishnan <karthik.b@gatech.edu>
2017-03-28 00:34:49 -0400

    Uncommented required lines

--- find_potential_malicious_sites.sh
+++ find_potential_malicious_sites.sh
@@ -13,30 +13,30 @@ LIST=$DATE\_potentially_malicious_sites
 
 # Download directly from the alexa page
 echo "Downloading alexa zip file"
-#wget http://s3.amazonaws.com/alexa-static/top-1m.csv.zip -O zip/$ZIP
+wget http://s3.amazonaws.com/alexa-static/top-1m.csv.zip -O zip/$ZIP
 
 # Extract zip to csv folder
 echo "Extracting zip file"
-#unzip -p zip/$ZIP > csv/$CSV
+unzip -p zip/$ZIP > csv/$CSV
 
 # Insert into DB
 echo "Inserting into database"
-#bash insert_into_db.sh $1 $2 $CSV
+bash insert_into_db.sh $1 $2 $CSV
 
 # Execute heuristic one
 echo "Applying heuristic - url appears once"
-#bash heuristic_url_appears_once.sh $1 $2
+bash heuristic_url_appears_once.sh $1 $2
 
 # Execute heuristic two
 echo "Applying heuristic - url rank drops"
 # date -v-1d works on Mac OSX but not Linux
 # python heuristic_url_rank_drops.py csv/`date -v-1d +"%m-%d-%y"`-top-1m-urls.csv csv/$CSV 250000 $DATE
-#python heuristic_url_rank_drops.py csv/`date +"%m-%d-%y" -d "yesterday"`-top-1m-urls.csv csv/$CSV 250000 $DATE
+python heuristic_url_rank_drops.py csv/`date +"%m-%d-%y" -d "yesterday"`-top-1m-urls.csv csv/$CSV 250000 $DATE
 
 # Merge lists
 echo "Merging list of websites obtained from heuristic"
-#mv $DATE\_url_rank_drops lists
-#mv $DATE\_url_appears_once lists
+mv $DATE\_url_rank_drops lists
+mv $DATE\_url_appears_once lists
 python merge_heuristic_lists.py lists/$LIST lists/$DATE\_url_appears_once lists/$DATE\_url_rank_drops
 
 # Split website list for containers
